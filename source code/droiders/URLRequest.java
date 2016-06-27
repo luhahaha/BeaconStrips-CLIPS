b8bc65b436ea39c0d2e07d902083cc8673238c27
Cenze94 <andrea.grendene94@gmail.com>
2016-06-27 12:47:49 +0200

    update URLRequest cambiata accessibilità campi dati in private

--- source code/droiders/URLRequest.java
+++ source code/droiders/URLRequest.java
@@ -7,17 +7,17 @@ import com.android.volley.*;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-//Questa classe è la superclasse che permette di comunicare con il server, il metodo execute() imposta la chiamata al server basandosi sulle variabili istanziate dal costruttore. Le sue sottoclassi semplicemente creano il body e impostano le variabili a seconda delle necessità. Il metodo execute() sarà usato da RequestMaker all'interno delle chiamate effettuate.
+//Questa classe è la superclasse che permette di comunicare con il server, il metodo execute() imposta la chiamata al server basandosi sulle variabili istanziate dal costruttore. Le sue sottoclassi semplicemente creano il body e impostano le variabili a seconda delle necessità tramite il costruttore di URLRequest. Il metodo execute() sarà usato da RequestMaker all'interno delle chiamate effettuate.
 
 //NOTA: per le callback bisogna capire come funzionano i Listener di volley, quindi probabilmente non servira' implementarle ma solo gestirle
 
 class URLRequest {
-	protected final Request.Method httpMethod; //da verificare se cosi' funziona, altrimenti bisogna capire se puo' funzionare in un altro modo, male che vada si fa un enum usando uno switch nell'execute()
-	protected final String url; //contiene l'url per la chiamata al server
-	protected final Map<String, String> headers; //contiene gli headers per la chiamata al server
-	protected final JSONObject body; //da verificare se effettivamente useremo questo tipo di oggetto
-	protected final URLRequestCallback callback; //da verificare se effettivamente useremo questo tipo di oggetto
-	protected final boolean requiresAuthentication; //true se viene richiesta l'autenticazione, false altrimenti
+	private final Request.Method httpMethod; //da verificare se cosi' funziona, altrimenti bisogna capire se puo' funzionare in un altro modo, male che vada si fa un enum usando uno switch nell'execute()
+	private final String url; //contiene l'url per la chiamata al server
+	private final Map<String, String> headers; //contiene gli headers per la chiamata al server
+	private final JSONObject body; //da verificare se effettivamente useremo questo tipo di oggetto
+	private final URLRequestCallback callback; //da verificare se effettivamente useremo questo tipo di oggetto
+	private final boolean requiresAuthentication; //true se viene richiesta l'autenticazione, false altrimenti
 
 	void execute() { //effettua la chiamata al server, sarà chiamato da RequestMaker, il tipo di ritorno sara' poi la callback (Listener o del tipo che definiamo noi)
 		StringRequest Request = new StringRequest(http.method, url, ) //TODO
