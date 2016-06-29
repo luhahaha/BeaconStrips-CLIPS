803a2536de68db0186f7e34f90316eed497d124b
mfranco91 <matteo.franco91@gmail.com>
2016-06-29 09:33:54 +0200

    Update: prova view registrazione

--- prove/view/Authentication/app/src/main/java/clips/matteo/authentication/MainActivity.java
+++ prove/view/Authentication/app/src/main/java/clips/matteo/authentication/MainActivity.java
@@ -15,12 +15,28 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private List users;
+    private EditText email;
+    private EditText username;
+    private EditText password;
+    private EditText repeatPassword;
+    private TextInputLayout errorEmail;
+    private TextInputLayout errorUsername;
+    private TextInputLayout errorPassword;
+    private TextInputLayout errorRepeatPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         users = new ArrayList();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email = (EditText) findViewById(R.id.email);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        repeatPassword = (EditText) findViewById(R.id.repeat_password);
+        errorEmail = (TextInputLayout) findViewById(R.id.email_label);
+        errorUsername = (TextInputLayout) findViewById(R.id.username_label);
+        errorPassword = (TextInputLayout) findViewById(R.id.password_label);
+        errorRepeatPassword = (TextInputLayout) findViewById(R.id.repeat_password_label);
         setButton();
     }
 
@@ -29,8 +45,33 @@ public class MainActivity extends AppCompatActivity {
         if (register != null) {
             register.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    Log.i("Event", "Button clicked");
-                    checkFields();
+                    if (checkFields()) {
+                        final EditText email = (EditText) findViewById(R.id.email);
+                        final EditText password = (EditText) findViewById(R.id.password);
+                        final EditText username = (EditText) findViewById(R.id.username);
+                        final EditText repeatPassword = (EditText) findViewById(R.id.repeat_password);
+
+                        users.add(new User(email.getText().toString(), username.getText().toString(), password.getText().toString()));
+                        Intent i = new Intent(getApplicationContext(), ConfirmRegistration.class);
+                        i.putExtra("email", email.getText().toString());
+                        i.putExtra("password", password.getText().toString());
+                        startActivity(i);
+                        clearFields();
+
+                        //finish(); se l'utente fa back qui esce dall'app
+                        /*
+                            questo va aggiunto al file AndroidManifest.xml per evitare di aggiungere l'attività allo stack
+                            <activity android:name=".SplashActivity" android:noHistory="true"/>
+                        */
+                        /*
+                            un'altra soluzione può essere implementare il metodo nell'altra attività
+                            @Override
+                            public void onBackPressed() {
+                            //e lasciarlo vuoto per non avere nessuna azione
+                            }
+                         */
+
+                    }
                 }
             });
         }
@@ -40,54 +81,61 @@ public class MainActivity extends AppCompatActivity {
                 public void onClick(View v) {
                     Intent i = new Intent(getApplicationContext(), LoginActivity.class);
                     startActivity(i);
-
                 }
             });
         }
     }
 
-    private void checkFields() {
-        final EditText email = (EditText) findViewById(R.id.email);
-        final EditText username = (EditText) findViewById(R.id.username);
-        final EditText password = (EditText) findViewById(R.id.password);
-        final EditText repeatPassword = (EditText) findViewById(R.id.repeat_password);
+
+
+    private void clearFields() {
+        email.requestFocus();
+        email.setText("");
+        username.setText("");
+        password.setText("");
+        repeatPassword.setText("");
+        errorEmail.setErrorEnabled(false);
+        errorUsername.setErrorEnabled(false);
+        errorPassword.setErrorEnabled(false);
+        errorRepeatPassword.setErrorEnabled(false);
+    }
+
+    private boolean checkFields() {
+
         boolean registration = true;
         if (email != null && username != null && password != null && repeatPassword != null) {
             if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email.getText()).matches()) {
-                TextInputLayout errorEmail = (TextInputLayout) findViewById(R.id.email_label);
                 errorEmail.setErrorEnabled(true);
                 errorEmail.setError("L'indirizzo email non è valido");
                 registration = false;
-                Log.i("Registration email", String.valueOf(registration));
             }
-            // TODO check su username già usato, lato server
+
+            if (username.getText().toString().isEmpty()) {
+                //espressione per username ([a-zA-Z0-9])+
+                errorUsername.setErrorEnabled(true);
+                errorUsername.setError("L'username non può essere vuoto");
+                registration = false;
+            }
 
             if (password.length() < 8) {
-                TextInputLayout errorPassword = (TextInputLayout) findViewById(R.id.password_label);
                 errorPassword.setErrorEnabled(true);
                 errorPassword.setError("La password è troppo corta");
                 registration = false;
-                Log.i("Password length", String.valueOf(registration));
             }
+
             else {
-                if (!password.getText().toString().equals(repeatPassword.getText().toString())) {
-                    System.out.println(password.getText().toString() + " " + repeatPassword.getText().toString());
-                    TextInputLayout errorPassword = (TextInputLayout) findViewById(R.id.password_label);
+                if (!(password.getText().toString().equals(repeatPassword.getText().toString()))) {
                     errorPassword.setErrorEnabled(true);
-                    errorPassword.setError("Le due password non corrispondono");
-                    TextInputLayout errorRepeatPassword = (TextInputLayout) findViewById(R.id.repeat_password_label);
                     errorRepeatPassword.setErrorEnabled(true);
+                    errorPassword.setError("Le due password non corrispondono");
                     errorRepeatPassword.setError("Le due password non corrispondono");
                     registration = false;
-                    Log.i("Password match", String.valueOf(registration));
                 }
-            }
-            if(registration)
-            {
-                users.add(new User(email.getText().toString(), username.getText().toString(), password.getText().toString()));
-                System.out.println("Size:" + users.size());
             }
         }
 
+        return registration;
+
     }
+
 }
