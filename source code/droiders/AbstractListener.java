40b6e85e6a7efa91c290ceca6d29e3e83e00f67c
Cenze94 <andrea.grendene94@gmail.com>
2016-06-30 17:51:11 +0200

    update URLRequest aggiunto abstract metodi AbstractListener

--- source code/droiders/AbstractListener.java
+++ source code/droiders/AbstractListener.java
@@ -1,6 +1,6 @@
 package client.request;
 
 abstract class AbstractListener { //la classe astratta da cui derivano i listener, onResponse e onError gestiscono le risposte rispettivamente quando viene ottenuta la risposta e quando viene ricevuto un errore
-	public onResponse(JSONObject response);
-	public onError(VolleyError error);
+	public abstract onResponse(JSONObject response);
+	public abstract onError(VolleyError error);
 }
