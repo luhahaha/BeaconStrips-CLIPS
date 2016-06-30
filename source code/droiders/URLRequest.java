252801341527b2b84e9c6d836a12343ae1817673
Cenze94 <andrea.grendene94@gmail.com>
2016-06-30 12:51:00 +0200

    add AbstractListener update URLRequest correzione classi

--- source code/droiders/URLRequest.java
+++ source code/droiders/URLRequest.java
@@ -7,51 +7,70 @@ import com.android.volley.*;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-//Questa classe è la superclasse che permette di comunicare con il server, il metodo execute() imposta la chiamata al server basandosi sulle variabili istanziate dal costruttore. Le sue sottoclassi semplicemente creano il body e impostano le variabili a seconda delle necessità tramite il costruttore di URLRequest. Il metodo execute() sarà usato da RequestMaker all'interno delle chiamate effettuate.
+//Questa classe è la superclasse che permette di comunicare con il server, il metodo execute() imposta la chiamata al server basandosi sulle variabili istanziate dal costruttore. Le sue sottoclassi semplicemente creano il body e impostano le variabili a seconda delle necessità tramite il costruttore di URLRequest. Il metodo execute() sarà usato da RequestMaker all'interno delle chiamate effettuate (o nelle sottoclassi).
 
-//NOTA: per le callback bisogna capire come funzionano i Listener di volley, quindi probabilmente non servira' implementarle ma solo gestirle
+//NOTA: il contratto della classe si conclude con la richiesta effettuata al server, si arrangia poi il Listener a gestire la risposta sia in caso positivo sia quando avviene un errore
+
+//ALTRA NOTA: per semplicita' ho inserito per ora l'URLRequest, l'AppInfoRequest e il RequestMaker in un unico file, successivamente bisognera' dividerli nei vari file necessari
 
 class URLRequest {
-	private final Request.Method httpMethod; //da verificare se cosi' funziona, altrimenti bisogna capire se puo' funzionare in un altro modo, male che vada si fa un enum usando uno switch nell'execute()
-	private final String url; //contiene l'url per la chiamata al server
-	private final Map<String, String> headers; //contiene gli headers per la chiamata al server
-	private final JSONObject body; //da verificare se effettivamente useremo questo tipo di oggetto
-	private final URLRequestCallback callback; //da verificare se effettivamente useremo questo tipo di oggetto
-	private final boolean requiresAuthentication; //true se viene richiesta l'autenticazione, false altrimenti
+	private Request.Method httpMethod; //da verificare se cosi' funziona, altrimenti bisogna capire se puo' funzionare in un altro modo, male che vada si fa un enum usando uno switch nell'execute()
+	private String url; //contiene l'url per la chiamata al server
+	private JSONObject body; //da verificare se effettivamente useremo questo tipo di oggetto
+	//qui va eventualmente inserita la callback
+	private AbstractListener listener;
 
 	void execute() { //effettua la chiamata al server, sarà chiamato da RequestMaker, il tipo di ritorno sara' poi la callback (Listener o del tipo che definiamo noi)
-		RequestQueue queue = Volley.newRequestQueue(this); //non so se in realtà questo campo dati sia uno di quelli dichiarati esternamente, ne dubito comunque
+		RequestQueue queue = Volley.newRequestQueue(this);
 		//ora viene creata la richiesta da fare
-		JsonObjectRequest Request = new JsonObjectRequest(httpMethod, url, null,
-				new.Response.Listener<JSONObject>() { //Hic sunt leones, ovvero da qui ho fatto una copia ma non ho un'idea precisa di cosa stia trattando, sicuramente sarà da rivedere e modificare
+		JsonObjectRequest request = new JsonObjectRequest(httpMethod, url, body,
+			new Response.Listener<JSONObject>() {
 			@Override
 			public void onResponse(JSONObject response) {
-				//Mostra i primi 500 caratteri della stringa response, sfrutto lo standard output generale per evitare di dover definire altre strutture, questa istruzione sarà sicuramente sostituita con qualcos'altro
-				System.out.println("La risposta è: "+ response.toString());
+				try {
+				//Mostra la stringa response, sfrutto lo standard output generale per evitare di dover definire altre strutture, questa istruzione sarà sicuramente sostituita con qualcos'altro (piu' probabilmente sara' eliminata)
+					System.out.println("La risposta è: "+ response.toString());
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				listener.onResponse(response);
 			}
 		}, new Response.ErrorListener() {
 			@Override
-			public void onErrorResponse(VolleyError error) { //qui la guida nel caso specifico del Json non aveva completato il metodo, probabilmente non sarà comunque il punto più difficile da gestire
+			public void onErrorResponse(VolleyError error) { //come in onResponse la riga sotto quasi sicuramente sara' eliminata
 				System.out.println("Niente da fare, qualcosa non va.");
+				listener(error);
 			}
-		}); //DA FINIRE/CORREGGERE
-		queue.add(Request); //qui viene aggiunta la richiesta appena creata
+		});
+		queue.add(request); //qui viene aggiunta la richiesta appena creata
 	}
 
-	URLRequest(Request.method httpMethod, String url, Map<String, String> headers, JSONObject body, boolean authentication) { //il costruttore inizializza i campi dati. La callback è standard per tutti, quindi non e' richiesta come input per il costruttore
-		this.httpMethod=httpMethod; //vengono inizializzati tutti i dati in base a quelli immessi nel costruttore
-		this.url=url;
-		this.headers=headers;
-		this.body=body;
-		//inizializzazione della callback
-		requiresAuthentication=authentication;
+	void addHeader() {
+		body.put("Authentication", Data.datamanager.LoginManager.sharedManager().getToken()); //qui viene aggiunto agli headers il token
+	}
+
+	URLRequest(Request.method httpMethod, String url, JSONObject body, boolean authentication, AbstractListener listener) { //il costruttore inizializza i campi dati. La callback è standard per tutti, quindi non e' richiesta come input per il costruttore. authentication è vera quando viene richiesta l'autenticazione, e quindi viene aggiunto l'header con il token dell'utente, falso altrimenti
+		this.httpMethod = httpMethod; //vengono inizializzati tutti i dati in base a quelli immessi nel costruttore
+		this.url = url;
+		this.headers = headers;
+		this.body = body;
+		if(authentication == true) { //se è richiesta l'autenticazione allora il token viene caricato nell'header del JSONObject body
+			addHeader();
+		}
+		this.listener = listener;
 	}
 }
 
-public class DiscoveryUUID { //classe di esempio, per ritornare il dato inserisco il costruttore dell'oggetto e un metodo getter per il campo dati, in alternativa posso usare un metodo statico che effettua la richiesta dato che posso crearci l'oggetto stesso all'interno
-	private String UUID; //qui verrà salvato il dato
-	public String getUUID() {return UUID;} //per poter usare questo metodo la classe dev'essere già costruita e quindi UUID sarà sicuramente non vuoto
-	DiscoveryUUID(){ //nel costruttore viene creato il JSON, viene inizializzata l'URLRequest e viene salvato il valore ottenuto con la chiamata al server
+class AppInfoRequest extends URLRequest { //classe che inizializza l'URLRequest in modo da poter effettuare la richiesta per ottenere sia l'UUID dei beacon sia le stringhe delle ingo generali dell'app
+	AppInfoRequest(AbstractListener listener){ //nel costruttore vengono creati il JSON (che in questo caso non c'è) e tutti gli altri dati, infine inizializzo con super() l'URLRequest
+		String url=URLDataConstants.baseURL + ""; //NOTA: l'url e' da finire
+		super(Request.method.GET, url, null, false, listener);
+		execute(); //effettuo la richiesta, lo inserisco qui visto che viene sempre chiamato dai metodi di RequestMaker (senno' basta inserirlo nei metodi stessi)
+	}
+}
 
+public class RequestMaker { //questa classe contiene i metodi per effettuare tutti i tipi di richieste al server richiesti all'interno dell'app
+	public static void getAppInfo(AbstractListener listener) { //effettua la chiamata per ottenere l'UUID dei beacon e le stringhe con le info dell'app
+		AppInfoRequest request = new AppInfoRequest(listener); //ULRequest viene automaticamente inizializzato
 	}
 }
