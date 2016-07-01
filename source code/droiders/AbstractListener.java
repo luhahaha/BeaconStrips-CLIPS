c85b5f0bc1a910970da1635bf0254bf672bcbe30
Cenze94 <andrea.grendene94@gmail.com>
2016-07-01 21:58:05 +0200

    update urlrequest fix

--- source code/droiders/AbstractListener.java
+++ source code/droiders/AbstractListener.java
@@ -5,7 +5,7 @@ import com.android.volley.VolleyError;
 import org.json.JSONObject;
 
 //la classe astratta da cui derivano i listener, onResponse e onError gestiscono le risposte rispettivamente quando viene ottenuta la risposta e quando viene ricevuto un errore
-abstract class AbstractListener {
+public abstract class AbstractListener {
 	public abstract void onResponse(JSONObject response);
 	public abstract void onError(VolleyError error);
 }
