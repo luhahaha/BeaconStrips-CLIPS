b5a9c99bf546a1a9c1cb093e983eb86fd59997d7
Kingwario <enrico.bellio7@gmail.com>
2016-07-06 11:52:25 +0200

    Droiders - LoginRequest fix

--- source code/droiders/LoginRequest.java
+++ source code/droiders/LoginRequest.java
@@ -11,8 +11,8 @@ import org.json.JSONObject;
  */
 public class LoginRequest extends URLRequest{
 
-   LoginRequest(String email, String password) {
-      super(Request.Method.POST, URLDataConstants.baseURL + "", setBody(email,password), false, new LoginRequestListener()); //l'url è da finire
+   LoginRequest(Context cx, String email, String password) {
+      super(cx, Request.Method.POST, URLDataConstants.baseURL + "", setBody(email,password), false, new LoginRequestListener()); //l'url è da finire
    }
 
    private static JSONObject setBody(String email, String password){
