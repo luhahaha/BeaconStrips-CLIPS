76ba17472ca309e702ad2fb9409eb27beef7a017
Cenze94 <andrea.grendene94@gmail.com>
2016-07-16 12:50:15 +0200

    add datamanager bozza DataRequestMaker update LoginManager2

---
+++ source code/droiders/Data/datamanager/DataRequestMaker.java
@@ -0,0 +1,40 @@
+package Data.datamanager;
+
+import android.content.Context;
+
+/**
+ * Created by andrea on 16/07/16.
+ */
+public class DataRequestMaker {
+   /*Questo codice sarà sotto commento finché non verranno implementate le classi chiamate
+
+
+   public static void getBuildings(Context cx, double latitude, double longitude, urlrequest.AbstractListener listener) {
+      BuildingsRequest(cx, latitude, longitude, listener);
+   }
+
+   public static void getPath(Context cx, int pathID, urlrequest.AbstractListener listener) {
+      PathDataRequest(cx, pathID, listener);
+   }
+
+   public static void getDiscoveryUUID(Context cx, urlrequest.AbstractListener listener) {
+      DiscoveryUUIDDataRequest(cx, pathID, listener);
+   }
+
+   public static void getRanking(Context cx, int pathID, urlrequest.AbstractListener listener) {
+      RankingRequest(cx, pathID, listener);
+   }
+
+   public static void getResults(Context cx, urlrequest.AbstractListener listener) {
+      GetResultsDataRequest(cx, listener);
+   }
+
+   public static void saveResult(Context cx, result pathResult, urlrequest.AbstractListener listener) {
+      SaveResultDataRequest(cx, pathResult, listener);
+   }
+
+   public static void getAppInfo(Context cx, urlrequest.AbstractListener listener) {
+      AppInfoDataRequest(cx, listener);
+   }
+    */
+}
--- source code/droiders/Data/datamanager/LoginManager2.java
+++ source code/droiders/Data/datamanager/LoginManager2.java
@@ -11,7 +11,7 @@ public class LoginManager2 {
    private Context cx; //serve per poter usare SharedPreferences, la classe che permette di usare il "dizionario" con i valori di base che ci interessano
 
    private LoginManager2(Context cx) {
-      this.cx=cx;
+      this.cx = cx;
    }
 
    public static LoginManager2 sharedManager(Context cx) {
@@ -29,18 +29,20 @@ public class LoginManager2 {
    }
 
    public String getToken() {
-      return cx.getSharedPreferences("LogData", Context.MODE_PRIVATE).getString("token", ""); //prelevo token dal file
+      return cx.getSharedPreferences("LogData", Context.MODE_PRIVATE).getString("token", ""); //prelevo token dal file, la stringa vuota indica cose ritornare se il token non c'è. MODE_PRIVATE è il modo standard con cui si lavora sui fogli tramite il codice, in alternativa si può usare MODE_APPEND
    }
 
    void login(String username, String password, urlrequest.AbstractListener listener) {
-      String token=new String(); //per ora do per scontato di averlo già ricevuto in input
-      SharedPreferences sp=cx.getSharedPreferences("LogData", Context.MODE_PRIVATE);
+
+      String token = new String(); //per ora do per scontato di averlo già ricevuto in input
+      SharedPreferences sp = cx.getSharedPreferences("LogData", Context.MODE_PRIVATE);
       sp.edit().putString("token", token);
       sp.edit().apply();
    }
 
    void logout(urlrequest.AbstractListener listener) {
-
+      urlrequest.RequestMaker.logout(cx, listener); //esegue la chiamata al server, qui non mi interessa se riesce o no
+      //TODO: da decidere se qui dev'essere cancellato il token, il momento è giusto ma il problema è che se avviene un errore con la comunicazione perdo il token, quindi devo rifare il login. La cosa più giusta da fare sarebbe cancellarlo solo quando la chiamata ha successo, il che vorrebbe dire aggiungere l'istruzione al listener. In alternativa potrei definire un listener intermedio astratto in cui lo obbligo a fare così
    }
 
    void register(String email, String username, String password, urlrequest.AbstractListener listener) {
@@ -51,3 +53,5 @@ public class LoginManager2 {
 
    }
 }
+
+//Dato che ogni metodo con una richiesta da fare all'urlrequest dovrebbe scomporre il JSONObject, si potrebbe fare la scomposizione in un listener astratto intermedio, come suggerito sopra; oppure il listener potrebbe chiamare un metodo di LoginManager a cui passa il JSON
