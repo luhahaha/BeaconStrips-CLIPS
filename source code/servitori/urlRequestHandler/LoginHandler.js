c75278fe926680155548ce9a8747ef395b84050f
tom139 <tommasopanozzo@gmail.com>
2016-07-26 12:41:05 +0200

    Fix: better login response and token gen
    Login response now includes user data and login makes use of token generator utility.

--- source code/servitori/urlRequestHandler/LoginHandler.js
+++ source code/servitori/urlRequestHandler/LoginHandler.js
@@ -2,6 +2,7 @@
 
 var db = require('./db.js');
 var URLRequestHandler = require('./URLRequestHandler.js');
+var Token = require('./utility/TokenGenerator.js');
 
 function BeaconRequest() {
    this.userID = function(email, password) {
@@ -11,7 +12,11 @@ function BeaconRequest() {
             password: password
          });
          query.then(function(result){
-            resolve(result[0]['id']);
+            if (result.hasOwnProperty(0) && result[0].hasOwnProperty('id')) {
+               resolve(result[0]['id']);
+            } else {
+               resolve();
+            }
          }, function(error) {
             console.log('failure with error ', error);
             reject(error);
@@ -38,13 +43,13 @@ function BeaconRequest() {
 
    */
 
-   this.generateToken = function() {
-      var uuid = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-         var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-         return v.toString(16);
-      });
-      return uuid;
-   };
+   // this.generateToken = function() {
+   //    var uuid = 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+   //       var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+   //       return v.toString(16);
+   //    });
+   //    return uuid;
+   // };
 
    /*
 
@@ -110,36 +115,71 @@ function BeaconRequest() {
    this.execute = function() {
       var email = this.request.body.email;
       var password = this.request.body.password;
-      console.log('email = ', email, '\npassword = ', password);
+      // console.log('email = ', email, '\npassword = ', password);
       var handler = this;
       this.userID(email, password).then(function(userID) {
          if (userID) {
-            console.log('esiste e l\'id è', userID);
+            // console.log('esiste e l\'id è', userID);
             // console.log('this = ', handler);
-            var token = handler.generateToken();
-            console.log('il token è ', token);
-            handler.response.status(200).send({
-               token: token
-            });
+            var token = Token.generate();
             var query = db()('AuthToken').insert({
                token: token,
                userID: userID,
                expirationDate: '2016-12-31'
             });
-            console.log('query = ', query.toSQL());
             query.then(function(result) {
-               console.log('result = ', result);
+               // console.log('il token appena generato è ', token);
+
+               // ottengo i dati (username e email) da tornare all'utente
+               var context = {
+                  response: handler.response,
+                  token: token
+               }
+               var userDataQuery = db().from('User').where({
+                  id: userID
+               });
+               userDataQuery.then(function(result) {
+                  if (result.hasOwnProperty(0)) {
+                     var data = result[0];
+                     var userData = {
+                        username : data.username,
+                        email    : data.email,
+                        id       : data.id,
+                        creation : data.creationDate,
+                        modification : data.modificationDate
+                     };
+                     context.response.status(200).send({
+                        token: context.token,
+                        userData: userData
+                     });
+                  } else {
+                     console.error('unable to get user with ID (', id, ') after saving a new token');
+                     context.response.status(505).send({
+                        errorCode: 505,
+                        debugMessage: 'dopo aver creato ed inserito il nuovo token per l\'utente, non sono più in grado di trovare i dati per l\'utente'
+                     });
+                  }
+               }.bind(context), function(error) {
+                  console.error('error looking for user\'s data after inserting a new token');
+                  response.status(505).send({
+                     errorCode: 505,
+                     debugMessage: 'error looking for user\'s data after inserting a new token'
+                  });
+               }.bind(context))
             }, function(error) {
-               console.log('error = ', error);
+               console.error('error = ', error);
+               handler.response.status(505).send({
+                  errorCode: 505,
+                  debugMessage: 'Errore cercando l\'userID dell\'utente che prova a fare il login'
+               });
             })
          } else {
-            console.log('non esiste!');
+            console.log('l\'utente non esiste!');
             handler.response.status(401).send({
                message: "l'utente non esiste"
             });
          }
       }, function(error) {
-         console.log('error = ', error);
          console.error('error = ', error);
       });
    };
