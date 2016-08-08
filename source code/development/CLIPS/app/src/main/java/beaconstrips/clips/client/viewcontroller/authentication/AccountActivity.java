b8f9538ad31612fd3edd511779959ae64822b89e
viviales <alessio.viviana@gmail.com>
2016-08-08 20:25:11 +0200

    add: modificato AccountActivity

--- source code/development/CLIPS/app/src/main/java/beaconstrips/clips/client/viewcontroller/authentication/AccountActivity.java
+++ source code/development/CLIPS/app/src/main/java/beaconstrips/clips/client/viewcontroller/authentication/AccountActivity.java
@@ -1,16 +1,42 @@
 package beaconstrips.clips.client.viewcontroller.authentication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 import beaconstrips.clips.R;
+import beaconstrips.clips.client.viewcontroller.savedresults.ResultActivity;
+import beaconstrips.clips.client.viewcontroller.utility.MenuActivity;
 
-public class AccountActivity extends AppCompatActivity {
+public class AccountActivity extends MenuActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
-        //TODO add paths and see results
+        setButtonInfoProfilo();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_menu_drawer, menu);
+        return true;
+    }
+
+    void setButtonInfoProfilo() {
+        final Button info = (Button) findViewById(R.id.results_button);
+        if (info != null) {
+            info.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(getApplicationContext(), ResultActivity.class);
+                    startActivity(i);
+                }
+            });
+        }
     }
 }
