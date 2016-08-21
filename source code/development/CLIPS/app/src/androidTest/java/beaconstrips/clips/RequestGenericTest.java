789aff38c89ba5e2aaa09c254ca91cd203aaf579
Cenze94 <andrea.grendene94@gmail.com>
2016-08-21 22:24:03 +0200

    update RequestGenericTest, manifest e build.gradle

--- source code/development/CLIPS/app/src/androidTest/java/beaconstrips/clips/RequestGenericTest.java
+++ source code/development/CLIPS/app/src/androidTest/java/beaconstrips/clips/RequestGenericTest.java
@@ -25,15 +25,14 @@ import beaconstrips.clips.client.urlrequest.URLDataConstants;
  * @author Andrea Grendene
  *
  * classe che contiene il TU (Test di Unità ). Verifica che la richiesta ad una pagina non esistente venga gestita correttamente, un caso possibile ad esempio se il server cambia indirizzo e il client non è ancora stato aggiornato.
- * Inoltre verifica che venga gestito correttamente il caso in cui l'applicazione perde l'accesso ad Internet, ad esempio perché il dispositivo si trova in un'area poco coperta dal proprio operatore telefonico.
- * Per il primo test verrà utilizzata come chiamata AppInfo, perché è la più semplice da implementare e una di quelle che genera meno situazioni diverse possibili.
+ * Per il test verrà utilizzata come chiamata AppInfo, perché è la più semplice da implementare e una di quelle che genera meno situazioni diverse possibili.
  *
  *
  * Stampa attesa per il test "wrongAddress": "Rilevato un errore in wrongAddress():"
  *                                           "Codice dell'errore: 1000"
  *                                           "Messaggio per l'utente: "
  *                                           "Messaggio di debug: Error server isn't a JSON, the error code is 404"
- * Nota: dato che l'errore 404 è un html e non un JSON viene lanciata un'eccezione quando si cerca di parsare la risposta, di conseguenza è previsto che venga creato un errore ad hoc per informare in modo sicuro l'applicazione.
+ * Nota: dato che l'errore 404 è un html e non un JSON viene lanciata un'eccezione quando si cerca di fare il parsing della risposta, di conseguenza è previsto che venga creato un errore ad hoc per informare in modo sicuro l'applicazione.
  */
 @RunWith(AndroidJUnit4.class)
 @SmallTest
@@ -51,13 +50,13 @@ public class RequestGenericTest {
       Context context = rule.getActivity().getBaseContext();
       RequestMaker.getAppInfo(context, new AbstractUrlRequestListener() {
          public void onResponse(JSONObject response) {
-            Log.d("AppInfoTest", "Chiamata wrongAddress() eseguita con successo");
+            Log.d("wrongAddress", "Chiamata wrongAddress() eseguita con successo");
          }
          public void onError(ServerError error) {
-            Log.d("AppInfoTest", "Rilevato un errore in wrongAddress():");
-            Log.d("AppInfoTest", "Codice dell'errore: " + error.errorCode);
-            Log.d("AppInfoTest", "Messaggio per l'utente: " + error.userMessage);
-            Log.d("AppInfoTest", "Messaggio di debug: " + error.debugMessage);
+            Log.d("wrongAddress", "Rilevato un errore in wrongAddress():");
+            Log.d("wrongAddress", "Codice dell'errore: " + error.errorCode);
+            Log.d("wrongAddress", "Messaggio per l'utente: " + error.userMessage);
+            Log.d("wrongAddress", "Messaggio di debug: " + error.debugMessage);
          }
       });
       field.set(address, "http://52.58.6.246:1234/"); //viene ripristinato l'indirizzo originale per sicurezza, così se ci sono altri test da eseguire non vengono compromessi. Il cambio viene effettuato subito dopo aver effettuato la richiesta perché l'indirizzo errato non serve più poi
