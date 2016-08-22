b8d1a450a6dbabf3dff585e01259127de46a58f1
viviales <alessio.viviana@gmail.com>
2016-08-22 13:32:11 +0200

    add: collegamento con LoginManager

--- source code/development/CLIPS/app/src/main/java/beaconstrips/clips/client/viewcontroller/authentication/LoginActivity.java
+++ source code/development/CLIPS/app/src/main/java/beaconstrips/clips/client/viewcontroller/authentication/LoginActivity.java
@@ -8,26 +8,94 @@
 
 package beaconstrips.clips.client.viewcontroller.authentication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import beaconstrips.clips.client.data.datamanager.AbstractDataManagerListener;
+import beaconstrips.clips.client.data.datamanager.LoginManager;
 import beaconstrips.clips.R;
+import beaconstrips.clips.client.urlrequest.ServerError;
 import beaconstrips.clips.client.viewcontroller.utility.MenuActivity;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email;
+    private EditText password;
+    private TextInputLayout missingEmail;
+    private TextInputLayout missingPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email = (EditText) findViewById(R.id.email_login);
+        password = (EditText) findViewById(R.id.password_login);
+        missingEmail = (TextInputLayout) findViewById(R.id.email_label);
+        missingPassword = (TextInputLayout) findViewById(R.id.password_label);
         setButton();
+        setLoginButton();
         //TODO add check fields
     }
+
+
+
+    void setLoginButton() {
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        if(loginButton != null) {
+            loginButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    String e = email.getText().toString();
+                    String p = password.getText().toString();
+                    if(areSet(e, p)) {
+                        LoginManager.sharedManager(getApplicationContext()).login(e, p, new AbstractDataManagerListener<Boolean>() {
+                            @Override
+                            public void onResponse(Boolean response) {
+                                Log.e("funz", "response");
+                            }
+
+                            @Override
+                            public void onError(ServerError error) {
+                                Log.e("funz", "error");
+                            }
+                        });
+                    }
+                }
+            });
+        }
+    }
+
+
+    boolean areSet(String email, String password) {
+        boolean e = false, p = false;
+        if(TextUtils.isEmpty(email)) {
+            missingEmail.setErrorEnabled(true);
+            missingEmail.setError("Riempire entrambi i campi");
+            e = true;
+        }
+
+        if(TextUtils.isEmpty(password)) {
+            missingPassword.setErrorEnabled(true);
+            missingPassword.setError("Riempire entrambi i campi");
+            p = true;
+        }
+
+        if(e || p) {
+            return false;
+        }
+        return true;
+    }
+
 
     void setButton() {
         final Button forgotPassword = (Button) findViewById(R.id.forgot_password_button);
