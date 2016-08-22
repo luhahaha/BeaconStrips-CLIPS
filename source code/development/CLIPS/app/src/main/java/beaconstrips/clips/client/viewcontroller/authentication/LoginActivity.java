23ee40c31edd057dfbca6e9d1975d60832608ce1
viviales <alessio.viviana@gmail.com>
2016-08-22 19:08:31 +0200

    fix: LoginActivity

--- source code/development/CLIPS/app/src/main/java/beaconstrips/clips/client/viewcontroller/authentication/LoginActivity.java
+++ source code/development/CLIPS/app/src/main/java/beaconstrips/clips/client/viewcontroller/authentication/LoginActivity.java
@@ -109,33 +109,3 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 }
-
-class ForgotPassword extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forgot_password);
-        setButton();
-    }
-
-    void setButton() {
-        final Button recoverPassword = (Button) findViewById(R.id.recover_password_button);
-        if (recoverPassword != null) {
-            recoverPassword.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    Intent i = new Intent(getApplicationContext(), RecoverPassword.class);
-                    startActivity(i);
-                }
-            });
-        }
-    }
-}
-
-class RecoverPassword extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirm_recover_password);
-        //TODO add what to do if error with recover
-    }
-}
