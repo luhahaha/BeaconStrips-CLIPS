359af5040b24d5c1d2099d70f1b5e195828297c9
mfranco91 <matteo.franco91@gmail.com>
2016-08-24 11:51:36 +0200

    Add: development - ricerca per quiz tramite beacon

--- source code/development/CLIPS/app/proguard-rules.pro
+++ source code/development/CLIPS/app/proguard-rules.pro
@@ -15,3 +15,15 @@
 #-keepclassmembers class fqcn.of.javascript.interface.for.webview {
 #   public *;
 #}
+-dontwarn okio.**
+-dontwarn retrofit2.**
+-keep class retrofit2.** { *; }
+-keepattributes Signature
+-keepattributes Exceptions
+
+-keepclasseswithmembers class * {
+    @retrofit2.http.* <methods>;
+}
+
+-keep class com.kontakt.sdk.** { *; }
+-keep interface com.kontakt.sdk.** { *; 
--- source code/development/CLIPS/app/src/main/java/beaconstrips/clips/client/viewcontroller/games/SearchNewStepActivity.java
+++ source code/development/CLIPS/app/src/main/java/beaconstrips/clips/client/viewcontroller/games/SearchNewStepActivity.java
@@ -13,42 +13,163 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.kontakt.sdk.android.ble.configuration.ActivityCheckConfiguration;
+import com.kontakt.sdk.android.ble.configuration.ScanPeriod;
+import com.kontakt.sdk.android.ble.configuration.scan.ScanMode;
+import com.kontakt.sdk.android.ble.connection.OnServiceReadyListener;
+import com.kontakt.sdk.android.ble.device.BeaconRegion;
+import com.kontakt.sdk.android.ble.filter.ibeacon.IBeaconFilters;
+import com.kontakt.sdk.android.ble.manager.ProximityManager;
+import com.kontakt.sdk.android.ble.manager.ProximityManagerContract;
+import com.kontakt.sdk.android.ble.manager.listeners.IBeaconListener;
+import com.kontakt.sdk.android.ble.manager.listeners.ScanStatusListener;
+import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleIBeaconListener;
+import com.kontakt.sdk.android.ble.manager.listeners.simple.SimpleScanStatusListener;
+import com.kontakt.sdk.android.common.KontaktSDK;
+import com.kontakt.sdk.android.common.profile.IBeaconDevice;
+import com.kontakt.sdk.android.common.profile.IBeaconRegion;
+
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import beaconstrips.clips.R;
 import beaconstrips.clips.client.viewcontroller.utility.MenuActivity;
 
 public class SearchNewStepActivity extends MenuActivity {
 
+    private ProximityManagerContract proximityManager;
+    private Button startTestButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_new_step);
+        startTestButton = (Button) findViewById(R.id.startTestButton);
+        setButtons();
+        startTestButton.setVisibility(View.INVISIBLE);
 
-        // prova statica
-        setButton();
         /* TODO when proof beacon is found:
             TextView descriptionLabel = (TextView) findElementById(R.id.descriptionLabel);
             TextView hintLabel = (TextView) findElementById(R.id.hintLabel);
             Button startTestButton = Button findElementById(R.id.startTestButton);
             descriptionLabel.setVisibility(View.GONE);
             hintLabel.setVisibility(View.GONE);
-            startTestButton.setVisibility(View.VISIBLE);
          */
-
+        KontaktSDK.initialize(this);
+        proximityManager = new ProximityManager(this);
+        configureProximityManager();
+        configureListeners();
+        configureSpaces();
+        //configureFilters();
         //TODO add all algorithms for proximity
     }
 
-    void setButton() {
-        final Button startTest = (Button) findViewById(R.id.startTestButton);
-        if (startTest != null) {
-            startTest.setOnClickListener(new View.OnClickListener() {
-                @Override
+    private void configureProximityManager() {
+        proximityManager.configuration()
+                .scanMode(ScanMode.LOW_LATENCY)
+                .scanPeriod(ScanPeriod.create(TimeUnit.SECONDS.toMillis(3), TimeUnit.SECONDS.toMillis(7)))
+                .activityCheckConfiguration(ActivityCheckConfiguration.MINIMAL)
+                .monitoringEnabled(false);
+    }
+
+    private void configureListeners() {
+        proximityManager.setIBeaconListener(createIBeaconListener());
+        proximityManager.setScanStatusListener(createScanStatusListener());
+    }
+
+    private void configureSpaces() {
+        IBeaconRegion region = new BeaconRegion.Builder()
+                .setIdentifier("All my iBeacons")
+                .setProximity(UUID.fromString("f7826da6-4fa2-4e98-8024-bc5b71e0893e"))
+                .build();
+
+        proximityManager.spaces().iBeaconRegion(region);
+    }
+
+    private void configureFilters() {
+        proximityManager.filters().iBeaconFilter(IBeaconFilters.newDeviceNameFilter(""));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        startScanning();
+    }
+
+    @Override
+    protected void onStop() {
+        proximityManager.stopScanning();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        proximityManager.disconnect();
+        proximityManager = null;
+        super.onDestroy();
+    }
+
+    private void startScanning() {
+        proximityManager.connect(new OnServiceReadyListener() {
+            @Override
+            public void onServiceReady() {
+                proximityManager.startScanning();
+            }
+        });
+    }
+
+    private void showToast(final String message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(SearchNewStepActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private IBeaconListener createIBeaconListener() {
+        return new SimpleIBeaconListener() {
+            @Override
+            public void onIBeaconDiscovered(IBeaconDevice ibeacon, IBeaconRegion region) {
+                if (ibeacon.getUniqueId().equals("JySs")) { //qui va modificato il beacon da trovare!
+                    Log.i("Sample", "IBeacon discovered! UniqueID: " + ibeacon.getUniqueId());
+                    Log.i("Distance", "" + ibeacon.getProximity());
+                    showToast("Hai trovato il beacon!");
+                    startTestButton.setVisibility(View.VISIBLE);
+
+                }
+            }
+        };
+    }
+
+    private ScanStatusListener createScanStatusListener() {
+        return new SimpleScanStatusListener() {
+            @Override
+            public void onScanStart() {
+                //showToast("Scanning started");
+            }
+
+            @Override
+            public void onScanStop() {
+                //showToast("Scanning stopped");
+            }
+        };
+    }
+
+
+    private void setButtons() {
+        if (startTestButton != null) {
+            startTestButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     Intent i = new Intent(getApplicationContext(), MultipleChoiceQuizActivity.class);
                     startActivity(i);
+                    //TODO add spinner for loading
+
                 }
             });
         }
