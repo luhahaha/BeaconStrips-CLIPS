48561f2b47ff526439ba4ffceee5adb51c12e9b4
viviales <alessio.viviana@gmail.com>
2016-08-25 18:00:50 +0200

    fix: layout AccountActivity, add: vettoriali icone

--- source code/development/CLIPS/app/src/main/java/beaconstrips/clips/client/viewcontroller/authentication/AccountActivity.java
+++ source code/development/CLIPS/app/src/main/java/beaconstrips/clips/client/viewcontroller/authentication/AccountActivity.java
@@ -16,20 +16,31 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import beaconstrips.clips.R;
+import beaconstrips.clips.client.data.LoggedUser;
+import beaconstrips.clips.client.data.datamanager.LoginManager;
 import beaconstrips.clips.client.viewcontroller.savedresults.ResultActivity;
 import beaconstrips.clips.client.viewcontroller.savedresults.SavedResultsActivity;
 import beaconstrips.clips.client.viewcontroller.utility.MenuActivity;
 
 public class AccountActivity extends MenuActivity {
-
+    private TextView email;
+    private TextView username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
         setButtonInfoProfilo();
+        email = (TextView) findViewById(R.id.account_email);
+        username = (TextView) findViewById(R.id.account_username);
+        if(LoginManager.sharedManager(getApplicationContext()).isLogged()){
+            LoggedUser u = LoginManager.sharedManager(getApplicationContext()).getLoggedUser();
+            email.setText(u.email);
+            username.setText(u.username);
+        }
     }
 
     void setButtonInfoProfilo() {
