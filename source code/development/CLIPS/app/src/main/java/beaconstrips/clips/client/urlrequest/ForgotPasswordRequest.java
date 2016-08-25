2e1dd17dcaad60bde00815d7d45ef4efaf63ff9d
Cenze94 <andrea.grendene94@gmail.com>
2016-08-25 12:17:02 +0200

    add urlrequest e datamanager chiamata ForgotPassword; update URLRequest check errore non connessione

---
+++ source code/development/CLIPS/app/src/main/java/beaconstrips/clips/client/urlrequest/ForgotPasswordRequest.java
@@ -0,0 +1,34 @@
+package beaconstrips.clips.client.urlrequest;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * @file LoginRequest.java
+ * @date 25/08/16
+ * @version 1.0.0
+ * @author Andrea Grendene
+ *
+ * classe derivata da URLRequest dove vengono impostati i dati per effettuare la richiesta al server di login
+ */
+public class ForgotPasswordRequest extends URLRequest {
+   ForgotPasswordRequest(Context cx, String email, String password, AbstractUrlRequestListener listener) {
+      super(cx, Request.Method.POST, URLDataConstants.baseURL + "login", setBody(email, password), false, listener);
+      execute(URLRequest.ResponseExpected.Object);
+   }
+
+   private static JSONObject setBody(String email, String password) {
+      JSONObject body = new JSONObject();
+      try {
+         body.put("email", email);
+         body.put("password", password);
+      } catch (JSONException e) {
+         signalError();
+      }
+      return body;
+   }
+}
