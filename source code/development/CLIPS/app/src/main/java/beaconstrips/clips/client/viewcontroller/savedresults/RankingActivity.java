fa7125c9ce00c8040b3f75e910b2971e1d3522e6
viviales <alessio.viviana@gmail.com>
2016-09-10 15:52:07 +0200

    add: layout RankingActivity + listView adapter

--- source code/development/CLIPS/app/src/main/java/beaconstrips/clips/client/viewcontroller/savedresults/RankingActivity.java
+++ source code/development/CLIPS/app/src/main/java/beaconstrips/clips/client/viewcontroller/savedresults/RankingActivity.java
@@ -10,14 +10,19 @@ package beaconstrips.clips.client.viewcontroller.savedresults;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import beaconstrips.clips.R;
 import beaconstrips.clips.client.data.Score;
 import beaconstrips.clips.client.data.datamanager.AbstractDataManagerListener;
 import beaconstrips.clips.client.data.datamanager.DataRequestMaker;
 import beaconstrips.clips.client.urlrequest.ServerError;
+import beaconstrips.clips.client.viewcontroller.utility.RankingAdapter;
+import beaconstrips.clips.client.viewcontroller.utility.risultatoProva;
 
 public class RankingActivity extends AppCompatActivity {
     ListView listView;
@@ -58,6 +63,44 @@ public class RankingActivity extends AppCompatActivity {
             }
         });
         */
+
+        ListView listView = (ListView)findViewById(R.id.playerList);
+        ArrayList lista = getListData();
+        //justifyListViewHeightBasedOnChildren(listView, lista);
+        listView.setAdapter(new RankingAdapter(this, lista));
     }
 
+    private ArrayList getListData() {
+        // prova di esempio, poi questi dati verranno presi dal DB
+        // è il ResultAdapter che mette gli elementi di row_result al posto giusto e dentro la list view
+        // fare un for per scorrere tutte le tappe
+        ArrayList<risultatoProva> results = new ArrayList<risultatoProva>();
+
+        risultatoProva ris = new risultatoProva();
+        ris.setData("10 Agosto");
+        ris.setDurata("2 minuti");
+        ris.setEdificio("Torre1");
+        ris.setPunteggio("20");
+        results.add(ris);
+
+        risultatoProva ris2 = new risultatoProva();
+        ris2.setData("10 Agosto");
+        ris2.setDurata("3 minuti");
+        ris2.setEdificio("Torre2");
+        ris2.setPunteggio("10");
+        results.add(ris2);
+
+        // Add some more dummy data for testing
+        return results;
+    }
+
+    // setta la dimensione della listView in base a quanti elementi ci sono nella lista
+    /*public static void justifyListViewHeightBasedOnChildren (ListView listView, ArrayList lista) {
+        int rows = lista.size();
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = rows*250;
+        listView.setLayoutParams(params);
+        listView.requestLayout();
+    }
+    */
 }
