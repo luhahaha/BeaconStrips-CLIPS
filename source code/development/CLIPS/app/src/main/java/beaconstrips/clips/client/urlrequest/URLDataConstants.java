a2bf39706c9ad18cdfd41af45fef88d520655883
Tommaso Panozzo <tommasopanozzo@gmail.com>
2016-09-11 23:47:22 +0200

    Update: base URL per chiamate al server in porta 80

--- source code/development/CLIPS/app/src/main/java/beaconstrips/clips/client/urlrequest/URLDataConstants.java
+++ source code/development/CLIPS/app/src/main/java/beaconstrips/clips/client/urlrequest/URLDataConstants.java
@@ -9,6 +9,6 @@ package beaconstrips.clips.client.urlrequest;
  * classe che contiene le informazioni relative alle richieste al server, ovvero l'indirizzo a cui accedere per effettuare le chiamate, serve per semplificare la modifica di questo valore, ad esempio quando il server viene trasferito
  */
 public class URLDataConstants {
-   final static String baseURL = new String("http://beaconstrips.tk:1234/");
+   final static String baseURL = new String("http://beaconstrips.tk/api/v1/");
    public final static String mediaBaseURL = new String("http://beaconstrips.tk/");
 }
